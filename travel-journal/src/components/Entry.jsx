472068695a0d371350b54099af5518d75336c78e
Jay Patel <jay43184@gmail.com>
2026-06-21 15:18:45 +0530

    change styling of travel journal

--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -5,11 +5,13 @@ export default function Entry(props){
                 <img src={props.img} alt={props.alt}className="entry-img"/>
             </div>
             <div className="entry-data">
-                <img 
-                src="/src/assets/marker.png" alt="marker-img" className="marker"
-                />
-                <span className="entry-location">{props.location}</span>
-                <a href={props.googleMapsUrl} className="entry-link">View on google maps</a>
+                <div className="entry-meta">
+                    <img 
+                    src="/src/assets/marker.png" alt="marker-img" className="marker"
+                    />
+                    <span className="entry-location">{props.location}</span>
+                    <a href={props.googleMapsUrl} className="entry-link">View on google maps</a>
+                </div>
                 <p className="entry-text">{props.title}</p>
                 <p className="entry-date">{props.date}</p>
                 <p className="entry-description">{props.description}</p>
