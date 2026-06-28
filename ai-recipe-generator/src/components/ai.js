462f6c5476653519a6c1bcb740f10af6bc5669f6
Jay Patel <jay43184@gmail.com>
2026-06-28 16:16:22 +0530

    add the base url for api endpoints

--- ai-recipe-generator/src/components/ai.js
+++ ai-recipe-generator/src/components/ai.js
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 const client = new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
+    dangerouslyAllowBrowser: true,
+    baseURL: import.meta.env.VITE_OPENAI_URL
 });
 
 
